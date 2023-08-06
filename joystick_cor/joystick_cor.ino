7bfe5d346e6b86e9d722df123c9c83fd4dfa37ec
ho942 <124801710+ho942@users.noreply.github.com>
2023-08-06 17:04:18 +0900

    joystick control

---
+++ joystick_cor/joystick_cor.ino
@@ -0,0 +1,106 @@
+const int buttonPin2 = 2;
+const int buttonPin3 = 3;
+const int buttonPin4 = 4;
+const int buttonPin5 = 5;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+pinMode(buttonPin2, INPUT_PULLUP);
+pinMode(buttonPin3, INPUT_PULLUP);
+pinMode(buttonPin4, INPUT_PULLUP);
+pinMode(buttonPin5, INPUT_PULLUP);
+
+
+
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int X = analogRead(0); // 0 523 1023 left right 
+int Y = analogRead(1); // 1023 509 0 up down
+
+int buttonValue2 = digitalRead(2); // A
+int buttonValue3 = digitalRead(3); // B
+int buttonValue4 = digitalRead(4); // C
+int buttonValue5 = digitalRead(5); // D
+// Default is High. Applying pressure makes it low 
+
+Serial.print("joy stick   ");
+Serial.print("X");
+Serial.print(":");
+Serial.print(X);
+Serial.print("  ");
+Serial.print("Y");
+Serial.print(":");
+Serial.println(Y);
+
+if (buttonValue2 == LOW) {
+
+  Serial.print("joy stick   ");
+Serial.print("X");
+Serial.print(":");
+Serial.print(X);
+Serial.print("  ");
+Serial.print("Y");
+Serial.print(":");
+Serial.print(Y);
+Serial.print("    \"");
+Serial.println("A: Yes   B: No  C: No  D : No");
+
+}
+
+if (buttonValue3 == LOW) {
+
+  Serial.print("joy stick   ");
+Serial.print("X");
+Serial.print(":");
+Serial.print(X);
+Serial.print("  ");
+Serial.print("Y");
+Serial.print(":");
+Serial.print(Y);
+Serial.print("    \"");
+Serial.println("A: No   B: Yes  C: No  D : No");
+
+}
+if (buttonValue4 == LOW) {
+
+  Serial.print("joy stick   ");
+Serial.print("X");
+Serial.print(":");
+Serial.print(X);
+Serial.print("  ");
+Serial.print("Y");
+Serial.print(":");
+Serial.print(Y);
+Serial.print("    \"");
+Serial.println("A: No   B: No  C: Yes  D : No");
+
+}
+
+if (buttonValue5 == LOW) {
+
+  Serial.print("joy stick   ");
+Serial.print("X");
+Serial.print(":");
+Serial.print(X);
+Serial.print("  ");
+Serial.print("Y");
+Serial.print(":");
+Serial.print(Y);
+Serial.print("    \"");
+Serial.println("A: No   B: No  C: No  D : Yes");
+
+}
+
+delay(500);
+
+}
